45c6bb0164c3a65c8f1d4eda7a677fdf7ebbd3fe
Scott Kirkland <srkirkland@gmail.com>
2011-05-10 10:12:54 -0700

    Added pages for a new html5 controller, which shows examples of input types and constraints

---
+++ ITSecuritySymposium/Controllers/Html5Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITSecuritySymposium.Controllers
+{
+    public class Html5Controller : Controller
+    {
+        //
+        // GET: /Html5/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InputTypes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InputTypes(string inputs)
+        {
+            ViewBag.Message = "Values Posted Successfully";
+
+            return View();
+        }
+
+        public ActionResult Constraints()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Constraints(string inputs)
+        {
+            ViewBag.Message = "Values Posted Successfully";
+
+            return View();
+        }
+    }
+}
