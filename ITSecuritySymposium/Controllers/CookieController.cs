0218ce387b85ddbdbcd259c6b2318a69ee9db962
Alan Lai <anlai@ucdavis.edu>
2011-06-06 09:11:54 -0700

    added cookie reading demo.

---
+++ ITSecuritySymposium/Controllers/CookieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITSecuritySymposium.Controllers
+{
+    public class CookieController : Controller
+    {
+        //
+        // GET: /Cookie/
+
+        public ActionResult Index()
+        {
+            var cookie = new HttpCookie("demo1", "Down with hypnotoad!");
+            var cookie2 = new HttpCookie("demo2", "All glory to the hypnotoad.");
+
+            // prevent client scripts from reading the cookie
+            cookie.HttpOnly = true;
+
+            HttpContext.Response.Cookies.Add(cookie);
+            HttpContext.Response.Cookies.Add(cookie2);
+
+            return View();
+        }
+
+    }
+}
