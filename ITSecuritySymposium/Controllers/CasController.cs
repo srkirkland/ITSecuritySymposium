af50b3dcbe4bb84645212d6c62ab998cc33cdbea
Scott Kirkland <srkirkland@gmail.com>
2015-06-16 21:05:50 -0700

    login to cas

---
+++ ITSecuritySymposium/Controllers/CasController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITSecuritySymposium.Controllers
+{
+    public class CasController : Controller
+    {
+        public ActionResult Index()
+        {
+            return CasMvc.LoginAndRedirect();
+        }
+    }
+}
